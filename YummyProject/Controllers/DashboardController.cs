ed57b313878b474f4a6fc70c5f0b93116057150f
Koray Vatansever <koray-vatansever@hotmail.com>
2025-03-01 21:24:58 +0300

    fix

--- YummyProject/Controllers/DashboardController.cs
+++ YummyProject/Controllers/DashboardController.cs
@@ -18,7 +18,9 @@ namespace YummyProject.Controllers
             ViewBag.mostExpensive = context.Products.OrderByDescending(x => x.Price).Select(x => x.ProductName).FirstOrDefault();
             ViewBag.avgPrice = context.Products.Average(x => x.Price);
             ViewBag.cheapestPrice = context.Products.OrderBy(x => x.Price).Select(x => x.ProductName).FirstOrDefault();
-            return View();
+            
+            var values = context.Products.OrderByDescending(x => x.ProductId).Take(10).ToList();
+            return View(values);
         }
     }
 }
