7ed5e393bef1deff55b30c9e502d5aa47ab72e05
Koray Vatansever <koray-vatansever@hotmail.com>
2025-03-03 17:28:58 +0300

    end

---
+++ YummyProject/Controllers/AdminProfileController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YummyProject.Context;
+using YummyProject.Models;
+
+namespace YummyProject.Controllers
+{
+    public class AdminProfileController : Controller
+    {
+        YummyContext context = new YummyContext();
+        public ActionResult Index()
+        {
+            var values = context.Admins.ToList();
+            return View(values);
+        }
+
+        public ActionResult AddAdminP()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdminP(Admin adminpr)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(adminpr);
+            }
+            context.Admins.Add(adminpr);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAdminP(int id)
+        {
+            var delvalue = context.Admins.Find(id);
+            context.Admins.Remove(delvalue);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UpdateAdminp(int id)
+        {
+            var values = context.Admins.Find(id);
+            return View(values);
+
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAdminp(Admin admnp)
+        {
+
+            var value = context.Admins.Find(admnp.AdminId);
+            value.NameSurname = admnp.NameSurname;
+            value.UserName = admnp.UserName;
+            value.Password = admnp.Password;
+            value.ImageUrl = admnp.ImageUrl;
+
+            if (!ModelState.IsValid)
+            {
+                return View(admnp);
+            }
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- YummyProject/Controllers/DashboardController.cs
+++ YummyProject/Controllers/DashboardController.cs
@@ -18,7 +18,12 @@ namespace YummyProject.Controllers
             ViewBag.mostExpensive = context.Products.OrderByDescending(x => x.Price).Select(x => x.ProductName).FirstOrDefault();
             ViewBag.avgPrice = context.Products.Average(x => x.Price);
             ViewBag.cheapestPrice = context.Products.OrderBy(x => x.Price).Select(x => x.ProductName).FirstOrDefault();
-            
+            ViewBag.maincourseCount = context.Products.Count(x => x.Category.CategoryName == "Ana Yemekler");
+            ViewBag.snackCount = context.Products.Count(x => x.Category.CategoryName == "Ara Sıcaklar");
+            ViewBag.dessertCount = context.Products.Count(x => x.Category.CategoryName == "Tatlılar");
+            ViewBag.drinkCount = context.Products.Count(x => x.Category.CategoryName == "İçecekler");
+            ViewBag.eventmostExpensive = context.Events.OrderByDescending(x => x.Price).Select(x => x.Title).FirstOrDefault();
+
             var values = context.Products.OrderByDescending(x => x.ProductId).Take(10).ToList();
             return View(values);
         }
